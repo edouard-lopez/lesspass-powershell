83d5785f9e945ecd61e08ecd735951b6ec7dc2fc
Edouard Lopez <contact@edouard-lopez.com>
2019-01-06 17:56:54 +0100

    restore test body for default profile

--- Profile/Profile.test.ps1
+++ Profile/Profile.test.ps1
@@ -5,17 +5,21 @@ Clear-Host
 Describe 'Profile' {
     Context "Create default profile" {
         ($profile, $master_pasword) = New-Profile "site.org" "my-login"
-        It 'contains default <properties>' -TestCases @(
-            @{property = "lowercase"}
-            @{property = "uppercase"}
-            @{property = "digits"}
-            @{property = "symbols"}
-            @{property = "length"}
-            @{property = "counter"}
-            @{property = "site"}
+
+        It 'contains default <property>' -TestCases @(
+            @{property = "lowercase"},
+            @{property = "uppercase"},
+            @{property = "digits"},
+            @{property = "symbols"},
+            @{property = "length"},
+            @{property = "counter"},
+            @{property = "site"},
             @{property = "login"}
-        ) {}
-        It 'contains given `site` and `login` value' {
+        ) {
+            param($property)
+            ($profile | Get-Member -MemberType NoteProperty).Name | should -Contain ($property)
+        }
+
             $profile.site | Should -Be 'site.org'
             $profile.login | Should -Be 'my-login'
         }
