c67af9a1f59f3312d817f3612c4e6531a8d1b232
Edouard Lopez <contact@edouard-lopez.com>
2019-04-05 23:02:33 +0200

    correct Get-LessPass test value

--- Lesspass.tests.ps1
+++ Lesspass.tests.ps1
@@ -3,7 +3,7 @@ Import-module $PSScriptRoot/Lesspass.psd1 -Force
 Describe 'Get-LessPass' {
     Context "Simple rule" {
         It 'Run' {
-            Get-LessPass "site" "login" "masterpassword"
+            Get-LessPass "site" "login" "masterpassword" | Should -BeExactly 'cp$=}`taN2LZ=PF@'
         }
     }
 }
