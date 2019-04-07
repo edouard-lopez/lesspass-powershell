2debe36dbbe5eb23883224874fcab5d5eb7b17c0
Edouard Lopez <contact@edouard-lopez.com>
2019-04-07 20:56:48 +0200

    add 'publish' task to publish module

--- makefile
+++ makefile
@@ -1,3 +1,7 @@
 .PHONY: test
 test:
 	pwsh -Command 'Invoke-Pester -EnableExit (Get-childItem -Recurse *.tests.ps1).fullname'
+
+.PHONY: publish
+publish:
+	pwsh -File ./tools/publish-module.ps1 "$$(pwd)" "${PSGALLERY_API_KEY}"
---
+++ tools/publish-module.ps1
@@ -0,0 +1,7 @@
+param($ProjectDirectory,$PSGALLERY_API_KEY)
+
+$Env:PSModulePath = $Env:PSModulePath + ":$ProjectDirectory"
+Publish-Module `
+    -Name "Lesspass" `
+    -Repository "PSGallery" `
+    -NuGetApiKey $Env:PSGALLERY_API_KEY `
